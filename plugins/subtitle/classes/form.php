b9ab949e0e0225ee5e1c8a57d2d66722b7d0a3fb
Smartlearn-edu <onlymade@gmail.com>
2026-01-26 19:09:32 +0200

    1- subtitle

---
+++ plugins/subtitle/classes/form.php
@@ -0,0 +1,128 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace ivplugin_subtitle;
+
+/**
+ * Class form
+ *
+ * @package    ivplugin_subtitle
+ * @copyright  2024 Antigravity
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class form extends \mod_interactivevideo\form\base_form
+{
+    /**
+     * Sets data for dynamic submission
+     * @return void
+     */
+    public function set_data_for_dynamic_submission(): void
+    {
+        $data = $this->set_data_default();
+
+        $draftitemid = file_get_submitted_draft_itemid('subtitlefile');
+
+        file_prepare_draft_area(
+            $draftitemid,
+            $data->contextid,
+            'mod_interactivevideo',
+            'subtitle',
+            $data->id,
+            ['subdirs' => 0, 'maxfiles' => 1]
+        );
+
+        $data->subtitlefile = $draftitemid;
+
+        // Restore other fields
+        $data->intg1 = $data->intg1 ?? 0;
+
+        $this->set_data($data);
+    }
+
+    /**
+     * Process dynamic submission
+     *
+     * @return void
+     */
+    public function process_dynamic_submission()
+    {
+        global $DB;
+        $fromform = $this->get_data();
+        $fromform = $this->pre_processing_data($fromform);
+        $fromform->advanced = $this->process_advanced_settings($fromform);
+
+        if ($fromform->id > 0) {
+            $fromform->timemodified = time();
+            $DB->update_record('interactivevideo_items', $fromform);
+        } else {
+            $fromform->timecreated = time();
+            $fromform->timemodified = $fromform->timecreated;
+            $fromform->id = $DB->insert_record('interactivevideo_items', $fromform);
+        }
+
+        // Save file
+        $draftitemid = file_get_submitted_draft_itemid('subtitlefile');
+        file_save_draft_area_files(
+            $draftitemid,
+            $fromform->contextid,
+            'mod_interactivevideo',
+            'subtitle',
+            $fromform->id,
+            ['subdirs' => 0, 'maxfiles' => 1]
+        );
+
+        // We update the record again just to be safe, though not strictly necessary if no new fields added
+        $DB->update_record('interactivevideo_items', $fromform);
+
+        return $fromform;
+    }
+
+    /**
+     * Form definition
+     *
+     * @return void
+     */
+    public function definition()
+    {
+        $mform = &$this->_form;
+
+        $this->standard_elements();
+
+        $mform->addElement('text', 'title', '<i class="bi bi-quote iv-mr-2"></i>' . get_string('title', 'mod_interactivevideo'));
+        $mform->setType('title', PARAM_TEXT);
+        $mform->setDefault('title', 'Subtitle');
+        $mform->addRule('title', get_string('required'), 'required', null, 'client');
+
+        // File Manager for Subtitle
+        $mform->addElement(
+            'filemanager',
+            'subtitlefile',
+            get_string('uploadfile', 'ivplugin_subtitle'),
+            null,
+            ['subdirs' => 0, 'maxbytes' => 1048576, 'maxfiles' => 1, 'accepted_types' => ['.vtt', '.srt']]
+        );
+
+        // Toggle for showing subtitle
+        $mform->addElement('selectyesno', 'intg1', get_string('showsubtitle', 'ivplugin_subtitle'));
+        $mform->setDefault('intg1', 1);
+
+        $this->display_options_field();
+        $this->advanced_form_fields([
+            'hascompletion' => false,
+        ]);
+        $this->close_form();
+    }
+}
---
+++ plugins/subtitle/classes/main.php
@@ -0,0 +1,163 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace ivplugin_subtitle;
+
+/**
+ * Class main
+ *
+ * @package    ivplugin_subtitle
+ * @copyright  2024 Antigravity
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class main
+{
+    /**
+     * Constructor.
+     */
+    public function __construct() {}
+
+    /**
+     * Check if the subtitle can be used.
+     * @return bool True if the subtitle can be used, false otherwise.
+     */
+    public function can_used()
+    {
+        return true;
+    }
+
+    /**
+     * Returns the property of the subtitle type
+     * @return array
+     */
+    public function get_property()
+    {
+        return [
+            'name' => 'subtitle',
+            'title' => get_string('subtitlecontent', 'ivplugin_subtitle'),
+            'icon' => 'bi bi-card-text',
+            'amdmodule' => 'ivplugin_subtitle/main',
+            'class' => 'ivplugin_subtitle\\main',
+            'form' => 'ivplugin_subtitle\\form',
+            'hascompletion' => false,
+            'hastimestamp' => true,
+            'allowmultiple' => true,
+            'hasreport' => false,
+            'description' => get_string('subtitledescription', 'ivplugin_subtitle'),
+            'author' => 'Antigravity',
+            'preloadstrings' => true,
+        ];
+    }
+
+    /**
+     * Get the content.
+     * @param array $arg The arguments.
+     * @return string The content.
+     */
+    public function get_content($arg)
+    {
+        global $CFG;
+        $id = $arg["id"];
+        $contextid = $arg["contextid"];
+
+        // Retrieve the file url
+        $fs = get_file_storage();
+        $files = $fs->get_area_files($contextid, 'mod_interactivevideo', 'subtitle', $id, 'sortorder, itemid, filepath, filename', false);
+        $fileurl = '';
+        if ($files) {
+            $file = reset($files);
+            $fileurl = \moodle_url::make_pluginfile_url(
+                $file->get_contextid(),
+                $file->get_component(),
+                $file->get_filearea(),
+                $file->get_itemid(),
+                $file->get_filepath(),
+                $file->get_filename()
+            )->out();
+        }
+
+        // We render a hidden container or specific interactive elements
+        // The actual display logic will be handled by JS reading the VTT file
+        // Pass the file URL in a data attribute
+        $showsubtitle = isset($arg['intg1']) && $arg['intg1'] == 1; // Storing visibility pref in intg1
+
+        $content = '<div class="ivplugin-subtitle-container ' . ($showsubtitle ? '' : 'd-none') . '" 
+                        data-subtitle-url="' . $fileurl . '" 
+                        data-show-subtitle="' . ($showsubtitle ? '1' : '0') . '">
+                        <div class="subtitle-text p-2 text-center bg-dark text-white opacity-75 rounded">
+                            <span class="iv-subtitle-current-text">...</span>
+                        </div>
+                    </div>';
+
+        return $content;
+    }
+
+    /**
+     * Copies interactive video data from one course module to another.
+     *
+     * @param int $fromcourse The ID of the source course.
+     * @param int $tocourse The ID of the destination course.
+     * @param int $fromcm The ID of the source course module.
+     * @param int $tocm The ID of the destination course module.
+     * @param mixed $annotation Additional annotation or metadata for the copy process.
+     * @param int $oldcontextid The ID of the old context.
+     * @return mixed
+     */
+    public function copy($fromcourse, $tocourse, $fromcm, $tocm, $annotation, $oldcontextid)
+    {
+        global $CFG;
+        $annotation = (object) $annotation;
+        // Handle related files "subtitle" field.
+        require_once($CFG->libdir . '/filelib.php');
+        $fs = get_file_storage();
+        $files = $fs->get_area_files($oldcontextid, 'mod_interactivevideo', 'subtitle', (int) $annotation->oldid, 'id ASC', false);
+        foreach ($files as $file) {
+            $filerecord = [
+                'itemid' => $annotation->id,
+                'contextid' => $annotation->contextid,
+            ];
+            $fs->create_file_from_storedfile($filerecord, $file);
+        }
+        return $annotation;
+    }
+
+    /**
+     * Get the content type.
+     * @return string The content type.
+     */
+    public function get_content_type()
+    {
+        return $this->get_property()['name'];
+    }
+
+    /**
+     * Get the icon.
+     * @return string The icon.
+     */
+    public function get_icon()
+    {
+        return $this->get_property()['icon'] ?? '';
+    }
+
+    /**
+     * Get the title.
+     * @return string The title.
+     */
+    public function get_title()
+    {
+        return $this->get_property()['title'] ?? '';
+    }
+}
