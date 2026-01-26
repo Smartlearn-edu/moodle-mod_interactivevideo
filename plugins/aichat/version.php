173119b7ab4c57e45064dcd134146c771f0d240e
Smartlearn-edu <onlymade@gmail.com>
2026-01-26 04:18:47 +0200

    a

---
+++ plugins/aichat/version.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details for ivplugin_aichat
+ *
+ * @package    ivplugin_aichat
+ * @copyright  2024 Sokunthearith Makara <sokunthearithmakara@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'ivplugin_aichat';
+$plugin->version = 2024092400;
+$plugin->requires = 2024041600; // Requires Moodle 4.4+ (for Core AI)
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = 'v1.0.0';
